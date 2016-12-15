334f824b69a8650f6cf0ec6cf42581915431f318
Hadi Asghari <hadiasghari@users.noreply.github.com>
2016-12-15 15:43:11 -0500

    Updated cookiewall demo

--- cookie-demo.py
+++ cookie-demo.py
@@ -1,30 +1,26 @@
 from automation import TaskManager, CommandSequence
 
 
-def parse_cookie_selectors(file_name):
+def parse_cookiewall_selectors(file_name):
     """Parse the cookie selectors and return them in data structure.
 
     The data structure maps FQDNs to a list of CSS selectors.  The empty string
     "" maps to a list of general CSS selectors that apply to many sites.
     """
 
-    unknown = 0
-
     # Maps a domain to a list of selectors.
     filterlist = dict()
+    unknown = 0
 
-    lines = []
     with open(file_name) as fd:
         for line in fd:
-            line = line.strip()
-
             # AdBlock's filter rules are complex, but we only care about a
             # subset: element hiding rules.  These rules are denoted by the
             # symbol "##".
             if "##" not in line:
                 unknown += 1
                 continue
-            domains, selectors = line.split("##")
+            domains, selectors = line.strip().split("##")
 
             # Commas denote a list of domains and selectors.
             domains = [s.strip() for s in domains.split(",")]
@@ -40,12 +36,12 @@ def parse_cookie_selectors(file_name):
 
     return filterlist
 
-file_name = "automation/Commands/cookie-selectors.dat"
-selectors = parse_cookie_selectors(file_name)
+COOKIEWALL_DAT = "./cookie-selectors.dat"
+selectors = parse_cookiewall_selectors(COOKIEWALL_DAT)
 
 # The list of sites that we wish to crawl
 NUM_BROWSERS = 1
-sites = ['http://www.derstandard.at']
+sites = ['http://www.derstandard.at', 'http://tudelft.nl', 'http://ad.nl', 'http://google.nl', 'http://google.com']
 
 # Loads the manager preference and 3 copies of the default browser dictionaries
 manager_params, browser_params = TaskManager.load_default_params(NUM_BROWSERS)
@@ -65,14 +61,14 @@ manager = TaskManager.TaskManager(manager_params, browser_params)
 
 # Visits the sites with all browsers simultaneously
 for site in sites:
-    command_sequence = CommandSequence.CommandSequence(site)
+    command_sequence = CommandSequence.CommandSequence(site, reset=True)
 
     # Start by visiting the page
     command_sequence.get(sleep=0, timeout=60)
 
     command_sequence.detect_cookie_banner(selectors, timeout=60)
 
-    manager.execute_command_sequence(command_sequence, index='**') # ** = synchronized browsers
+    manager.execute_command_sequence(command_sequence)  
 
 # Shuts down the browsers and waits for the data to finish logging
 manager.close()
